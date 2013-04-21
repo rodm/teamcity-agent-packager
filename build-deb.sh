320964d57bbcd1d14dbffdc13a7ba4fc88a5c1cb
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2013-04-21 19:40:12 +0100

    Build script now expects 2 args one a URL to the build agent zip file to download and the second the version number

--- build-deb.sh
+++ build-deb.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
+if [ $# -ne 2 ]; then
+    echo "usage: `basename $0` <build agent url> <version>"
+    exit 1
+fi
+
+AGENT_URL=$1
+VERSION=$2
+
 SRC_FILE=buildAgent.zip
 BUILD_DIR=build
 PKG_DIR=$BUILD_DIR/pkg
-VERSION=4.5.6
 
-rm -rf $PKG_DIR
+rm -rf $BUILD_DIR || exit $?
 
+mkdir -p $BUILD_DIR || exit $?
 mkdir -p $PKG_DIR/DEBIAN
 mkdir -p $PKG_DIR/etc/init.d
 mkdir -p $PKG_DIR/usr/share/teamcity-agent
 mkdir -p $PKG_DIR/opt/teamcity-agent
-unzip -q $SRC_FILE -d $PKG_DIR/opt/teamcity-agent
+
+wget $AGENT_URL -O $BUILD_DIR/$SRC_FILE || exit $?
+unzip -q $BUILD_DIR/$SRC_FILE -d $PKG_DIR/opt/teamcity-agent
 
 # copy files
 cp src/teamcity-agent.init $PKG_DIR/etc/init.d/teamcity-agent
